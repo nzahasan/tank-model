a764e765d9113566642f320002a298e456ad3be1
Nazmul Ahasan <nzahasan@gmail.com>
2025-12-19 19:16:53 +0600

    requirements.txt and test setup fixes

--- load-env.sh
+++ load-env.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# load model environment for testing purposes only
+
 BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
 
 SCRIPTS_DIR=$BASE_DIR/scripts
